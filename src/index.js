3f349da8e806e5912b06f6a9c2c8ba438091e567
Teodora Vermesan <vermesanteodora@gmail.com>
2024-01-04 13:27:54 +0200

    render teams

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import "./style.css";
 
+function getTeamAsHTML(team) {
+  return `<tr>
+  <td>${team.activity}</td>
+  <td>${team.domain}</td>
+  <td>${team.details}</td>
+  <td>${team.status}</td>
+  <td>x</td>
+</tr>`;
+}
+
+function renderTeams(teams) {
+  const teamsHTML = teams.map(getTeamAsHTML);
+  document.querySelector("#teamsTable tbody").innerHTML = teamsHTML.join("");
+}
+
 function loadTeams() {
   fetch("teams.json")
     .then(r => r.json())
     .then(teams => {
-      console.warn("teams?", teams);
+      renderTeams(teams);
     });
 }
 loadTeams();
