9bfd3031720bb834ede4fa4512316c89a32cfbef
Teodora Vermesan <vermesanteodora@gmail.com>
2024-01-04 13:00:21 +0200

    load teaams.json and print teams in console

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import "./style.css";
 
-console.info("start app");
+function loadTeams() {
+  fetch("teams.json")
+    .then(r => r.json())
+    .then(teams => {
+      console.warn("teams?", teams);
+    });
+}
+loadTeams();
