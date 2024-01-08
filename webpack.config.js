d1c3df6f76010df57257f96143142adcac553d4c
Teodora Vermesan <vermesanteodora@gmail.com>
2024-01-08 16:08:00 +0200

    sdfds

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = env => {
       ]
     },
     output: {
-      filename: "main.js",
+      filename: "index.js",
       path: path.resolve(__dirname, "docs"),
       publicPath: ""
     }
